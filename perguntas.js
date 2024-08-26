75765adc7a7b643e354e5ef0e6db105947395c5d
Bernardo H. Iensen <107139060+EllonTusk@users.noreply.github.com>
2024-08-26 08:45:09 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+export const perguntas ={ 
+  
+  
+   const: caixaPrincipal = document.querySelector(caixa-principal),
+    const: caixaPerguntas = document.querySelector(caixa-perguntas),
+     const: caixaAlternativas = document.querySelector(caixa-alternativas),
+      const: caixaResultado = document.querySelector(caixa-resultado),
+       const: textoResultado = document.querySelector(texto-resultado),
+  }
+    
+     caixaAlternativas:[
+       
+      {  texto:  "Isso é assustador!",
+        afirmacao: (
+        "No início ficou com medo do que essa tecnologia pode fazer "
+      ,  "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+     )
+    },
+        { texto:  " é maravilhoso!",
+        afirmacao: (
+        "Quis saber como usar IA no seu dia a dia.",
+        " Pensou que IA pode ajudar em tarefas da sua vida."
+        )}
+
+   ]
+  if (sim) {
+    [[[[[[[[[[[[[]]]]]]]]]]]]]
+  }
